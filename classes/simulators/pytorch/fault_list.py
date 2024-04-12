bb673b73ffe4205817de1c511d26dfcc34ed3b98
Dario Passarello <dario.passarello@gmail.com>
2024-04-12 12:33:45 +0200

    docs: fault list

--- classes/simulators/pytorch/fault_list.py
+++ classes/simulators/pytorch/fault_list.py
@@ -64,6 +64,7 @@ class PyTorchFaultListInfo:
             }
 
 class PyTorchFaultList:
+
     def __init__(
         self,
         network: nn.Module,
@@ -96,10 +97,20 @@ class PyTorchFaultList:
             profiler_filter
         )
 
-        self.injectable_layers = self.get_injectable_layers_names()
+        self.injectable_layers = self.get_injectable_submodules_names()
         self.num_injectable_layers = len(self.injectable_layers)
 
-    def get_injectable_layers_names(self) -> List[str]:
+    def get_injectable_submodules_names(self) -> List[str]:
+        """
+        Gets the list of all the names of all submodules layers of the target network.
+        
+        A submodules is injectable if and only if the ``module_to_fault_generator_fn`` tied to this
+        instance returns a ``FaultGenerator`` when called on that submodule.
+
+        Returns
+        ---
+        A list of strings containing the fully qualified names of all the injectable layers of the network.
+        """
         names = []
         for name, module in self.network.named_modules():
             fault_generator = self.module_to_fault_generator_fn(name, module)
@@ -110,6 +121,35 @@ class PyTorchFaultList:
     def module_fault_list_generator(
         self, module_name : str, n_faults: int, fault_batch_size: int = 1
     ):
+        """
+        Creates a generator that generates lazily a finite number of faults faults for a specific module.
+        The faults are generated using the fault generator related to the module accordingly to
+        the ``module_to_fault_generator_fn`` tied to this instance. 
+
+        Args
+        ---
+        * ``module_name : str``: The complete, fully qualifed name of the submodule to generate faults for. 
+        * ``n_faults: int.``: Number of faults to generate.
+        * ``fault_batch_size: int``: The size of each group (batch) of faults generated at once.
+
+        Returns
+        ---
+        A generator object that could be iterated through to generate lazily faults.
+        Each iteration returns a tuple of three elements:
+        * The name of the module itself (equal to ``module_name`` every time)
+        * The number of the iteration
+        * A FaultBatch object containing ``fault_batch_size`` faults.
+
+        The generator stops after generating ``n_faults``, and will be empty if there is no
+        fault generator object associated to the module.
+
+        Raises
+        ---
+        ``AttributeError``: If ``module_name`` references an invalid
+        path to a module or resolves to something that is not an
+        ``nn.Module``.
+        """
+        
         module = self.network.get_submodule(module_name)
         n_iters = int(math.ceil(n_faults / fault_batch_size))
         fault_generator = self.module_to_fault_generator_fn(module_name, module)
@@ -128,7 +168,29 @@ class PyTorchFaultList:
     def network_fault_list_generator(
         self, n_faults: int, fault_batch_size: int = 1
     ) -> Generator[Tuple, Any, None]:
+        """
+        Creates a generator that generates lazily a finite number of faults faults for each injectable module of the network.
+        A module is injectable if it is associated to a ``FaultGenerator`` object returned by
+        the ``module_to_fault_generator_fn`` tied to this instance. 
 
+        The generator traverses all the submodules with the order of the ``torch.nn.Module.named_modules`` method.
+
+        Args
+        ---
+        * ``n_faults: int.``: Number of faults to generate for each module.
+        * ``fault_batch_size: int``: The size of each group (batch) of faults generated at once.
+
+        Returns
+        ---
+        A generator object that could be iterated through to generate lazily faults.
+        Each iteration returns a tuple of three elements:
+        * The name of the module for which the fault was generated
+        * The number of the batch relative to the current submodule. The numbering restarts for every submodule.
+        * A FaultBatch object containing ``fault_batch_size`` faults.
+
+        The generator stops after generating ``n_faults`` for each injectable submodule, and will skip
+        non-injectable modules.
+        """
         # To generalize this class to be used keras we should generalize the iterator for the layers of the network
         # You can use keras.Model._flatten_layers model to iterate trough all layers of the model
         # recursively, and then for getting the name you can access the layer .name attribute
@@ -137,19 +199,53 @@ class PyTorchFaultList:
             yield from self.module_fault_list_generator(name, n_faults, fault_batch_size)
     
 
-    def create_and_save_fault_list(
+    def generate_and_persist_fault_list(
         self,
         output_path: str,
-        n_faults: int,
+        n_faults_per_module: int,
         fault_batch_size: int = 1,
         show_progress=True,
         exists_ok=True,
         overwrite=False
     ):
+        """
+        Generates the faults of the faultlist according to the error models and saves the faults to be used later in an error simulation campaign.
+        Persists all those faults of the faultlist into a tar file that can be loaded using the ``FaultListFromTarFile``, similarly to how 
+        a traditional PyTorch input dataset is loaded.
+        The faults are not all stored in RAM and saved all once but are rather saved in a streamed fashion
 
-        if n_faults % fault_batch_size != 0:
-            raise ValueError(f'Number of faults per modules (n_faults) must be multiple of fault_batch size. Instead found {n_faults=} {fault_batch_size=}')
+        The output tar file is structured as it follows:
+        - ``fault_list.json``: A json file containing the basic info of the fault list. This file contains the data in the info member
+            of this class, of type PyTorchFaultListInfo.
+        - For each module of the model there is a folder containing various .npz files, each one contains a batch of faults of size ``fault_batch_size``.
+          The files in the module folder are named with this format ``"{batch_num}_faults_{module_name}.npz"``
         
+        Args
+        ---
+        * ``output_path : str``. Path where the output tar file is saved. Note that while saving the file a temp folder could be created for storing temporarly the 
+            produced files.
+        * ``n_faults : int``. Number of faults to generate for each module of the network. The total number of faults generated is simply ``n_faults * n_modules``.
+        * ``fault_batch_size : int``. The size of the batch stored in each npz file. For example 128 faults with a batch size of 8 are saved in 16 files. It must
+            be a divisor of ``n_faults``. This parameters affects heavily the size of the final archive, so in presence of disk space constraints 
+            it should be chosen accurately.
+        * ``show_progress : bool``. Draws a progress bar when generating and saving faults. Defaults to ``True``.
+        * ``exists_okay : bool``. If ``False`` a FileExistsError will be thrown if a fault list with the same output path already exists. Defaults to ``True``.
+        * ``overwrite : bool``. If ``False`` the function will not generate a fault list and gracefully return if a fault list with the same output path already exists. Defaults to ``True``.
+
+        Returns
+        ---
+        ``None``
+
+        Raises
+        ---
+        * ``ValueError``. When ``fault_batch_size`` does not divide ``n_faults``
+        * ``FileExistsError``. If ``exists_ok`` is ``False`` and a fault list at ``output_path`` already exists. This error is raised regardless of the value of ``overwrite``.
+        """
+
+        if n_faults_per_module % fault_batch_size != 0:
+            raise ValueError(f'Number of faults per modules (n_faults) must be multiple of fault_batch size. Instead found {n_faults_per_module=} {fault_batch_size=}')
+        
+        # Remove .tar from temp dir if present, add it to the output path if not present
         if output_path.endswith('.tar'):
             temp_output_dir = output_path[:-4] + '_tmp'
         else:
@@ -162,33 +258,31 @@ class PyTorchFaultList:
             if not overwrite:
                 return
 
-
         try:
             os.makedirs(temp_output_dir, exist_ok=False)
             count = 0
-            n_iters = int(math.ceil(n_faults / fault_batch_size)) * self.num_injectable_layers
+            n_iters = int(math.ceil(n_faults_per_module / fault_batch_size)) * self.num_injectable_layers
 
             pbar = None
             if show_progress:
                 pbar = tqdm(total=n_iters)
-            
-            self.info = PyTorchFaultListInfo(
+            # Create info object and be store it as the fault list descriptor
+            info = PyTorchFaultListInfo(
                 input_shape=list(self.input_shape),
                 batch_dimension=self.batch_axis,
                 modules_output_shapes=self.modules_output_shapes,
-                n_faults_per_module=n_faults,
-                injectable_layers=self.injectable_layers,
-                fault_batch_size=fault_batch_size,
-            )
+                n_faults_per_mod
 
             with open(os.path.join(temp_output_dir, 'fault_list.json'),'w') as f:
-                json.dump(self.info.to_dict(), f)
+                json.dump(info.to_dict(), f)
 
 
             for name, module in self.network.named_modules():
-                for module_name, batch_num, fault_batch in self.module_fault_list_generator(name, n_faults, fault_batch_size):
+                # Generate faults lazily and save them to the temp directory
+                for module_name, batch_num, fault_batch in self.module_fault_list_generator(name, n_faults_per_module, fault_batch_size):
                     if pbar:
-                        pbar.set_description(module_name)
+                        pbar.set_description(f'Generating Faults')
+                        pbar.set_postfix_str(module_name)
                     module_path = os.path.join(temp_output_dir, module_name)
                     os.makedirs(module_path, exist_ok=True)
 
@@ -200,13 +294,10 @@ class PyTorchFaultList:
                         "sp_classes": fault_batch.spatial_pattern_names,
                         "sp_parameters": fault_batch.sp_parameters,
                         "module": np.asarray(module_name),
-                        "seq": np.int64(count),
-                    }
-                    np.savez_compressed(file_name, **npz_dict)
-                    count += 1
+                        "seq": np
                     if pbar:
                         pbar.update(1)
-
+            # Archive all files
             with tarfile.TarFile(output_path, 'w') as tarf:
                 tarf.add(os.path.join(temp_output_dir, 'fault_list.json'), arcname='fault_list.json')
                 for module_folder in os.listdir(temp_output_dir):
@@ -216,4 +307,5 @@ class PyTorchFaultList:
                             batch_file_path = os.path.join(module_folder_path, batch_file)
                             tarf.add(batch_file_path, arcname=os.path.join(module_folder, batch_file))
         finally:
+            # Delete in any case the temp directory, even when a crash happens
             shutil.rmtree(temp_output_dir, ignore_errors=True)
